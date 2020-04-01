52da00c067971638db1ae7f318267505f36cf601
Hasancan <fatihmehmetsayin@gmail.com>
2020-04-02 01:25:54 +0300

    DataConfigurationYapılması ModellerinGüncellenmesi

---
+++ Hirdavat.Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Hirdavat.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hirdavat.Data
+{
+    class AppDbContext : DbContext
+    {
+
+        //AppDbcontext mümkün olduğunca sade olması gerekir   
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+       
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+
+  
+}
