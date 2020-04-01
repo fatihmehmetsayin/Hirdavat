e3689ebf91b6b91d5aa6c310cd384722bf83fadc
Hasancan <fatihmehmetsayin@gmail.com>
2020-04-01 18:12:34 +0300

    Yorum satırı eklendi

--- Hirdavat.Core/Repositories/IproductRepository.cs
+++ Hirdavat.Core/Repositories/IproductRepository.cs
@@ -10,7 +10,7 @@ namespace Hirdavat.Core.Repositories
     //generik repository pattern implement edildi   
     interface IproductRepository:IRepository<Product>
     {
-
+        //product da ait özel metotlar burada  olacak
         //Ürünün ID sine bağlı kadegoride  gelsin
 
         Task<Product> GetWithCategoryByIdAsync(int product);
