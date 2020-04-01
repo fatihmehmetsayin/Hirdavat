b9bc25f4ff9a6fe9748863e636f845d6b4488e98
Hasancan <fatihmehmetsayin@gmail.com>
2020-04-01 18:11:18 +0300

    Product ve category e özgü metotlaarın interfacelari yazıldı  Repository İnterface yazıldı

---
+++ Hirdavat.Core/Repositories/IproductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hirdavat.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hirdavat.Core.Repositories
+{
+
+    //generik repository pattern implement edildi   
+    interface IproductRepository:IRepository<Product>
+    {
+
+        //Ürünün ID sine bağlı kadegoride  gelsin
+
+        Task<Product> GetWithCategoryByIdAsync(int product);
+    }
+}
